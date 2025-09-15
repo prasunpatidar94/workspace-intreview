26e90910e687df8c6e7c33b8a973a41a8a4556a8
Prasun_Patidar <52647586+prasunpatidar94@users.noreply.github.com>
2025-09-16 00:38:49 +0530

    config server new path and euresever added

---
+++ prepration_code_01/src/main/java/com/pp/sun/program/StreamCode.java
@@ -0,0 +1,18 @@
+package com.pp.sun.program;
+
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.List;
+import java.util.function.Function;
+import java.util.stream.Collectors;
+
+public class StreamCode {
+
+    public static void main(String[] args) {
+
+        List<String> listIn =Arrays.asList("i","am", "prasun","patidar");
+//        listIn.stream().collect(Collectors.toCollection(Function.identity(),Collectors.counting()).c)
+
+    }
+}
